f314c621a92c86a69bf826120fe3e4856a835b53
ntun7729 <ntun77.19@gmail.com>
2026-01-05 17:24:57 +0630

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-# Start frpc first
+# Start frpc silently
 frpc -c /etc/frp/frpc.toml >/dev/null 2>&1 &
 
-# Start 3x-ui
-exec /app/x-ui
+# Silence syslog warning
+export DISABLE_SYSLOG=1
+
+# Start 3x-ui completely silent
+exec /app/x-ui >/dev/null 2>&1
